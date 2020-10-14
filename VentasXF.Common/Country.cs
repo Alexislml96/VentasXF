4bcb7a786a85634f23d95cfc3f344ed0d3831403
Alexislml96 <32379788+Alexislml96@users.noreply.github.com>
2020-10-14 14:47:49 -0500

    Creación de estructura

---
+++ VentasXF.Common/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace VentasXF.Common
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
---
+++ VentasXF.Web/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace VentasXF.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+    }
+}
+}
